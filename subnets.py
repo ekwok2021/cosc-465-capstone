752732134500a62038257abb384a9f94272e167d
ekwok2021 <ekwok@colgate.edu>
2022-04-04 23:44:07 +0000

    commit

---
+++ subnets.py
@@ -0,0 +1,14 @@
+class subnets():
+    def addressRanges():
+        return
+
+    
+    def subnetMembership():
+        return
+
+    def main():
+        print("DEBUG")
+        return
+    
+    if __name__ == "__main__":
+        main()
