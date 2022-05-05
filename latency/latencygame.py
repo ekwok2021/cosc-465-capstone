8dca9ca42181a60e21670569ae8bfb5c516ff475
ekwok2021 <89984772+ekwok2021@users.noreply.github.com>
2022-05-05 12:57:49 -0400

    Update latencygame.py

--- latency/latencygame.py
+++ latency/latencygame.py
@@ -1,5 +1,4 @@
 import pygame
-from subnets import subnets
 from latency import latency
 
 # ideas: Time -- scores
@@ -40,18 +39,16 @@ def button(screen, position, text):
     pygame.draw.rect(screen, (100, 100, 100), (x, y, w , h))
     return screen.blit(text_render, (x, y))
 
-def redrawGameWindow(score, LIVES, color_1, color_2, input_rect_1, input_rect_2, user_text_1, user_text_2, sub, timer_text, B, distance, bandwidth):
-
+def redrawGameWindow(score, LIVES, color_1, color_2, input_rect_1, input_rect_2, user_text_1, timer_text, B, distance, bandwidth):
     win.blit(back,(0,0))
-
     tip_font = pygame.font.SysFont("Ariel", 18, False)
-
     # update question
-
-    q_text = "Given " + str(B) + "B packet, distance = " + str(distance) + "km, and bandwidth = " + str(bandwidth) + "Gbps, latency = ?ms"
+    q_text = "What is the latency to send a " + str(B) + "B packet over a " + str(distance) + "km distance" 
+    q_text2 = "with a bandwidth of " + str(bandwidth) + "Gbps?"
     question = font.render(q_text, 1 ,(0,0,0))
+    question2 = font.render(q_text2, 1 ,(0,0,0))
     win.blit(question, (WIDTH/2 - 350, HEIGHT / 2 -50))
-    
+    win.blit(question2, (WIDTH/2 - 350, HEIGHT / 2 - 25))
 
     tip_text = tip_font.render("Press 'Enter' to submit", 1, (0,0,0))
     win.blit(tip_text, (WIDTH / 2 - 50, HEIGHT - 30))
@@ -63,54 +60,34 @@ def redrawGameWindow(score, LIVES, color_1, color_2, input_rect_1, input_rect_2,
     # update score and life
     lives_text = font.render("Score: " + str(score), 1, (0,0,0))
     win.blit(lives_text, (WIDTH - 150,10))
-
     score_text = font.render("LIVES: " + str(LIVES), 1, (255,0,0))
     win.blit(score_text, (50,10))
 
     # user input area 1 update
     in_text_1 = font.render("Latency = ", 1, (0,0,255))
     win.blit(in_text_1, (INPUT_HOR - 100, INPUT_VER))
+    in_text_ms = font.render("ms", 1, (0,0,255))
+    win.blit(in_text_ms, (INPUT_HOR + 110, INPUT_VER))
 
     pygame.draw.rect(win, color_1, input_rect_1)
-  
     text_surface_1 = base_font.render(user_text_1, True, (255, 255, 255))
 
-    # user input area 2 update
-    #in_text_2 = font.render("End range: ", 1, (0,0,255))
-    #win.blit(in_text_2, (INPUT_HOR - 200, INPUT_VER + 40))
-
-    #pygame.draw.rect(win, color_2, input_rect_2)
-  
-    #text_surface_2 = base_font.render(user_text_2, True, (255, 255, 255))
-
-      
     # render at position stated in arguments
     win.blit(text_surface_1, (input_rect_1.x+5, input_rect_1.y+5))
-    #win.blit(text_surface_2, (input_rect_2.x+5, input_rect_2.y+5))
-      
+
     # set width of textfield
-    input_rect_1.w = max(100, text_surface_1.get_width()+10)
-    #input_rect_2.w = max(100, text_surface_2.get_width()+10)
-      
+    input_rect_1.w = max(100, text_surface_1.get_width()+10)      
     
     # timer
     win.blit(font.render(timer_text, True, (0,0,0)),(WIDTH/2 + 50, 10))
 
     # update the screen
     pygame.display.flip()
-
     clock.tick(60)
 
-def resultGameWindow(timeout, correct, range1, range2, answer):
-    # change ranges from array to string
-
-    str_r1 = ".".join(map(str,range1))
-
-    str_r2 = ".".join(map(str,range2))
-
+def resultGameWindow(timeout, correct, answer):
     while True:
         win.blit(back,(0,0))
-
         if correct:
             result_text = result_font.render("Your Answer is CORRECT", 1, (0,0,0))
             win.blit(result_text, (100, HEIGHT / 2 - 100))
@@ -132,15 +109,6 @@ def resultGameWindow(timeout, correct, range1, range2, answer):
         clock.tick(60)
         pygame.display.update()
 
-# returns an array of 1) subnets in array form 2) subnets in string form
-def generateSubnet():
-    sub = subnets.genSubnet()
-    last = str(sub[-1])
-    s1 = sub[:-1]
-    str_sub = ".".join(map(str,s1)) + "/" + last
-    return [sub,str_sub]
-
-
 def GAME():
     level = MENU
     # menu page setup
@@ -152,7 +120,6 @@ def GAME():
         menu_font = pygame.font.SysFont("comicsans", 50, True)
         menu_text = menu_font.render("Latency Game " + "CLICK 'PLAY'", 1, (0,0,0))
         win.blit(menu_text, (50, HEIGHT / 2 - 100))
-
         for event in pygame.event.get():
             # close the pygame when close window
             if event.type == pygame.QUIT:
@@ -162,29 +129,22 @@ def GAME():
                     level = GAME
                 if button_quit.collidepoint(event.pos):
                     pygame.quit()
-
         pygame.display.update()
-
-
      # color when input box is clicked
     color_active = pygame.Color("blue")
-
     # color when input box is idle
     color_passive = pygame.Color('paleturquoise1')
-
     user_text_1 = ""
     user_text_2 = ""
     #user input area
     input_rect_1 = pygame.Rect(INPUT_HOR,INPUT_VER,140,32)
     input_rect_2 = pygame.Rect(INPUT_HOR - 30,INPUT_VER + 50 ,140,32)
-
     color_1 = color_passive
     color_2 = color_passive
     active_1 = False
     active_2 = False
     score = 0
     LIVES = 3
-    sub = generateSubnet()
     counter = MAXTIME
     pygame.time.set_timer(pygame.USEREVENT, 1000)
     timer_text = str(counter).rjust(3)
@@ -201,15 +161,13 @@ def GAME():
         propDelay = latency.calcPropDelay(distance)
         transDelay = latency.calcTransDelay(B, bandwidth)
         answer = latency.calcLat(propDelay, transDelay)
-        range1, range2 = subnets.addressRanges(sub[0])
 
         if counter <= 0:
             timeout = True
             LIVES = LIVES - 1
-            resultGameWindow(timeout, correct, range1, range2, answer)
+            resultGameWindow(timeout, correct, answer)
             counter = MAXTIME
             timer_text = str(counter).rjust(3)
-            sub = generateSubnet()
 
 
         for event in pygame.event.get():
@@ -238,17 +196,10 @@ def GAME():
                         user_text_1 = user_text_1[:-1]
                     else: 
                         user_text_1 += event.unicode
-                elif active_2:
-                    if event.key == pygame.K_BACKSPACE:
-                        user_text_2 = user_text_2[:-1]
-                    else:
-                        user_text_2 += event.unicode
-
                 if event.key == pygame.K_RETURN:
-                    range1, range2 = subnets.addressRanges(sub[0])
                     if timeout:
                         LIVES -= 1
-                        resultGameWindow(timeout, correct, range1, range2, answer)
+                        resultGameWindow(timeout, correct, answer)
 
                     # compare the answer
 
@@ -258,7 +209,6 @@ def GAME():
 
                     # change scores and clear answer
                     user_text_1 = ""
-                    user_text_2 = ""
                     if correct and not timeout:
                         print("CORRECT")
                         if counter >= 30:
@@ -269,10 +219,9 @@ def GAME():
                         LIVES -= 1
                     
                     # display result window
-                    resultGameWindow(timeout, correct, range1, range2, answer)
+                    resultGameWindow(timeout, correct, answer)
                     counter = MAXTIME
                     timer_text = str(counter).rjust(3)
-                    sub = generateSubnet()
                     B = latency.genB()
                     distance = latency.distance()
                     bandwidth = latency.bandwidth()
@@ -288,9 +237,7 @@ def GAME():
             if LIVES == 0:
                 level = END
 
-        # win.blit(timer_font.render(timer_text, True, (0,0,0)), (32,48))
-        # pygame.display.flip()
-        redrawGameWindow(score, LIVES, color_1, color_2, input_rect_1, input_rect_2, user_text_1, user_text_2, sub, timer_text, B, distance, bandwidth)
+        redrawGameWindow(score, LIVES, color_1, color_2, input_rect_1, input_rect_2, user_text_1, timer_text, B, distance, bandwidth)
 
     while level == END:
         clock.tick(60)
